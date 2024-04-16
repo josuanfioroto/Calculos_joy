a6405ad93d566ac879490f92377b2a8b25771c29
josuan_fioroto <142553191+josuanfioroto@users.noreply.github.com>
2024-04-16 19:18:13 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,17 @@
+const a = 4 , b = 5
+
+function operacoesBasicas(a, b) {
+   
+    console.log(a + ' + ' + b + ' = ' + (a + b));
+
+    
+    console.log(a + ' - ' + b + ' = ' + (a - b));
+
+  
+    console.log(a + ' x ' + b + ' = ' + (a * b));
+
+ 
+    console.log(a + ' / ' + b + ' = ' + (a / b));
+}
+
+operacoesBasicas(4, 5);
